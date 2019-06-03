11f9bc5a4fa9987c390280f426832cc72d79bc8f
Nícolas Bruno <nicolas_bruno@terceiros.sicredi.com.br>
2019-06-02 21:02:40 -0300

    crud projetos

--- client/src/components/projetos/App.js
+++ client/src/components/projetos/App.js
@@ -18,7 +18,24 @@ class Projetos extends Component {
     }    
 
     handleSubmit = character => {
-        this.setState({characters: [...this.state.characters, character]});
+        fetch("http://localhost:1234/projetos/", {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, cors, *same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrer: 'no-referrer', // no-referrer, *client
+            body: JSON.stringify(character), // body data type must match "Content-Type" header
+        })
+        .then(
+            response => response.json()
+        );
+        
+        this.setState({characters: [...this.state.characters, character]});    
     }
 
     componentDidMount() {
